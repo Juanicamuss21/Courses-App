6bc37654198294c928e42f89ab24d77b04f80c4b
Juanicamuss21 <juanicamuss15@gmail.com>
2023-08-10 19:24:59 -0300

    Version-1-project

--- api/src/controllers/controlVideos.js
+++ api/src/controllers/controlVideos.js
@@ -1,4 +1,3 @@
-const axios = require('axios')
 const {Videos} = require('../db')
 const videos = require('../videos.json')
 
@@ -15,10 +14,38 @@ const getAllVideos = async () => {
     }
 
         const allVideos = await Videos.findAll();
-         return allVideos;   
+        return allVideos;   
     }
         
         
-        module.exports = {getAllVideos}
+    module.exports = {getAllVideos}
 
+// const fs = require('fs');
+// const path = require('path');
+// const { Videos } = require('../db')
+
+// const getAllVideos = async () => {
+//     try {
+//         // Leer el archivo JSON
+//         const filePath = path.join(__dirname, '../videos.json');
+//         const data = fs.readFileSync(filePath, 'utf-8');
+//         const videos = data
+    
+//         for (const el of videos) {
+//             const images =JSON.stringify(el.images);
+//             await Videos.findOrCreate({
+//                 where: {image: el.image, images: images, category: el.category, title: el.title, autor: el.autor, description: el.description, github: el.github, documentation: el.documentation},
+//                 });
+//         }
+
+//         const allVideos = await Videos.findAll();
+//         return allVideos;
+    
+//       } catch (error) {
+//         console.error('Error al guardar los videos', error);
+//         throw Error(`Error: ${error}`)
+//       } 
+//   };
+
+//   module.exports = {getAllVideos}
   
